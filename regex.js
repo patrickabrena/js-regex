238ae488f8b6c558fb110e4e9a1300a72e853d28
Patrick Abrena <patrickabrena@hotmail.com>
2024-02-07 13:05:10 -0600

    got up to match beginning string patterns

--- regex.js
+++ regex.js
@@ -127,3 +127,74 @@ const matchLettersOfTheAlphabet = () => {
   console.log(result);
 };
 matchLettersOfTheAlphabet();
+
+const matchNumbersAndLettersOfTheAlphabet = () => {
+  //hypen can also work on numbers
+  //eg. /[0-5]/ matches any number between 0-5 inclding 0 and 5
+
+  let quoteSample = "Blueberry 3.141592653s are delicious.";
+  let myRegex = /[h-s2-6]/gi; // Change this line
+  let result = quoteSample.match(myRegex); // Change this line
+  console.log(result);
+};
+matchNumbersAndLettersOfTheAlphabet();
+
+const matchSingleCharactersNotSpecified = () => {
+  //can create a negated character set using caret (^)
+  //eg. /[^aeiou]/ this regex matches all chaaracters INCLUDING white space, ., !, @
+  let quoteSample = "3 blind mice.";
+  let myRegex = /[^aeiou0-9]/gi; // Change this line
+  let result = quoteSample.match(myRegex); // Change this line
+  console.log(result);
+};
+matchSingleCharactersNotSpecified();
+
+const matchCharactersThatOccurOneOrMoreTimes = () => {
+  //use + operator
+  let difficultSpelling = "Mississippi";
+  let myRegex = /s+/g; // Change this line
+  let result = difficultSpelling.match(myRegex);
+  console.log(result);
+};
+matchCharactersThatOccurOneOrMoreTimes();
+
+const matchCahractersThatOccurZeroOrMoreTimes = () => {
+  //use * asterisk for zero or more times
+  // Only change code below this line
+  let chewieQuote = "Aaaaaaaaaaaaaaaarrrgh!";
+  let chewieRegex = /Aa*/; // Change this line
+  // Only change code above this line
+  let result = chewieQuote.match(chewieRegex);
+  console.log(result);
+};
+matchCahractersThatOccurZeroOrMoreTimes();
+
+const findCharactersWithLazyMatching = () => {
+  //greedy match will find the longest possible part of a string that fits the regex pattern
+  //lazy match will find the smallest possible part of the string that satisfies the regex pattern
+  //default for matching is greedy
+  let titanicString = "titanic";
+  let greedyRegex = /t[a-z]*i/;
+  //greedyRegex is a pattern that starts with t and ends with i and has the full alphabet inbetween zero or more times. will return "titani"
+  let lazyRegex = /t[a-z]*?i/;
+  //with the question mark after the character set with asterix will return "ti" because the next letter in the titanic string is i and lazy regex will find the smallest possible part of string that satisfies the regex pattern
+  let greedyResult = titanicString.match(greedyRegex);
+  let lazyResult = titanicString.match(lazyRegex);
+  console.log(greedyResult, lazyResult);
+  //
+  //
+  //
+  let text = "<h1>Winter is coming</h1>";
+  // let myRegex = /<.*>/; // this regex will return the entire opening and closing tag with text inside it beacuse of greedy match
+  let myRegex = /<.*?>/; // this regex will match
+  let result = text.match(myRegex);
+  console.log(result);
+};
+findCharactersWithLazyMatching();
+const findOneOrMoreCriminalsInAHunt = () => {
+  //aanswer is hidden in the variable
+  //one or more instances is the + operator
+};
+findOneOrMoreCriminalsInAHunt();
+
+const matchBeginningStringPatterns = () => {};
