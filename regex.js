612578ea972208e27b6e973e1c526fba3b46a6a5
Patrick Abrena <patrickabrena@hotmail.com>
2024-02-12 22:23:03 -0600

    last one! finish tmr

--- regex.js
+++ regex.js
@@ -190,12 +190,13 @@ const findCharactersWithLazyMatching = () => {
   let result = text.match(myRegex);
   console.log(result);
 };
-findCharactersWithLazyMatching();
+// findCharactersWithLazyMatching();
+
 const findOneOrMoreCriminalsInAHunt = () => {
   //aanswer is hidden in the variable
   //one or more instances is the + operator
 };
-findOneOrMoreCriminalsInAHunt();
+// findOneOrMoreCriminalsInAHunt();
 
 const matchBeginningStringPatterns = () => {
   //inside character set, caret omits characters /[^aeiou]/
@@ -205,7 +206,7 @@ const matchBeginningStringPatterns = () => {
   let result = calRegex.test(rickyAndCal);
   console.log(result); //will return true
 };
-matchBeginningStringPatterns();
+// matchBeginningStringPatterns();
 
 const matchEndingStringPatterns = () => {
   //can search for patterns at the end of the string using dollar sign at end of regex
@@ -215,7 +216,7 @@ const matchEndingStringPatterns = () => {
   let result = lastRegex.test(caboose);
   console.log(result); // will return true
 };
-matchEndingStringPatterns();
+// matchEndingStringPatterns();
 
 const matchAllLettersAndNumbers = () => {
   //There is a shorthand for creating a character set
@@ -226,7 +227,7 @@ const matchAllLettersAndNumbers = () => {
   let result = quoteSample.match(alphabetRegexV2).length;
   console.log(result);
 };
-matchAllLettersAndNumbers();
+// matchAllLettersAndNumbers();
 
 const MATCH_EVERYTHING_BUT_LETTERS_AND_NUMBERS = () => {
   //shorthand for non-alphanumeric characters = /\W/
@@ -235,7 +236,7 @@ const MATCH_EVERYTHING_BUT_LETTERS_AND_NUMBERS = () => {
   let result = quoteSample.match(nonAlphabetRegex).length;
   console.log(result);
 };
-MATCH_EVERYTHING_BUT_LETTERS_AND_NUMBERS();
+// MATCH_EVERYTHING_BUT_LETTERS_AND_NUMBERS();
 
 const MATCH_ALL_NUMBERS = () => {
   //shortcut for character class [0-9] is \d
@@ -245,7 +246,7 @@ const MATCH_ALL_NUMBERS = () => {
   let result = movieName.match(numRegex)[0].length; //// will return 4
   console.log(result);
 };
-MATCH_ALL_NUMBERS();
+// MATCH_ALL_NUMBERS();
 
 const MATCH_ALL_NON_NUMBERS = () => {
   //shorthand for [^0-9] is \D
@@ -254,7 +255,7 @@ const MATCH_ALL_NON_NUMBERS = () => {
   let result = movieName.match(noNumRegex).length;
   console.log(result);
 };
-MATCH_ALL_NON_NUMBERS();
+// MATCH_ALL_NON_NUMBERS();
 
 const RESTRICT_POSSIBLE_USERNAMES = () => {
   //usernames can only use alphanumeric
@@ -320,3 +321,114 @@ const SPECIFY_EXACT_NUMBER_OF_MATCHES = () => {
   console.log(result);
 };
 SPECIFY_EXACT_NUMBER_OF_MATCHES();
+
+const CHECK_ALL_OR_NONE = () => {
+  // can specify the possible existence of element with question mark
+  /*eg. let american = "color
+        let british = "colour" 
+        let regex = /colou$r/ /// the $ after the u will check if it's there in the string and match it if it does
+        regex.test(american) would return true
+        regex.test(british) would return true as well 
+        */
+  let favWord = "favorite";
+  let favRegex = /favou?rite/; // Change this line
+  let result = favRegex.test(favWord);
+  console.log(result);
+};
+CHECK_ALL_OR_NONE();
+
+const POSITIVE_AND_NEGATIVE_LOOKAHEADS = () => {
+  //positive lookahead (?=...) will make sure the element in the search pattern is there BUT won't actually match it.
+
+  //negative lookadhea (?!...) will look ahead to make sure element in the pattern is not there. Rest of tthe pattern is returned if negative lookahead part is not present
+
+  const EXAMPLE_ONE = () => {
+    let quit = "qu";
+    let noquit = "qt";
+    let regexP = /q(?=u)/;
+    let regexN = /q(?!u)/;
+    let lookAheadPos = quit.match(regexP); // will log "q"
+    let lookAheadNeg = noquit.match(regexN); // will log "q"
+    console.log(lookAheadPos, lookAheadNeg);
+  };
+  EXAMPLE_ONE();
+
+  //practical use of lookaheads is to check two or more patterns in on string i.e a password checker
+  //below is a simple password that looks for between 3-6 characters and at least one number
+
+  const EXAMPLE_TWO = () => {
+    let password = "abc123";
+    let checkPassword = /(?=\w{3,6})(?=\D*\d)/;
+    //first lookahead is checking for between 3 to 6 (including 3 and 6) alpha-numeric charcters
+    //second lookahead is positive checking zero or more non-number at beginning of string (the \D*)  and then the rest having at least one number
+    let result = checkPassword.test(password);
+    console.log(result);
+  };
+  EXAMPLE_TWO();
+
+  let sampleWord = "astronaut";
+  let pwRegex = /(?=\w{6,})(?=\D*\d{2})/; // Change this line
+  //first Positive Look Ahead does positive check for any alpha-numeric character
+  //second P.L.A checks if string has zero or more non-digits followed by 2 digits
+  let answer = pwRegex.test(sampleWord);
+  console.log(answer);
+};
+POSITIVE_AND_NEGATIVE_LOOKAHEADS();
+
+const CHECK_FOR_MIXED_GROUPING_OF_CHARACTERS = () => {
+  // want to find either Penguin or Pumpkin in string?
+  // use RegEx: /P(engu|umpk)in/
+  let myString = "Eleanor Roosevelt";
+  let myRegex = /(Eleanor|Franklin D.) Roosevelt/; // Change this line
+  let result = myRegex.test(myString); // Change this line
+  console.log(result);
+};
+CHECK_FOR_MIXED_GROUPING_OF_CHARACTERS();
+
+const REUSE_PATTERNS_USING_CAPTURE_GROUPS = () => {
+  // capture groups are made by closing the regex pattern to be captured
+  // the substring matched by the group is saved a temp var which can be accessed within same regex using \1
+  /*
+  let repeatRegex = /(\w+) \1 \1/;
+repeatRegex.test(repeatStr); // Returns true
+repeatStr.match(repeatRegex); // Returns ["row row row", "row"] 
+*/
+
+  //Capture group n amount of times
+  const CAPTURE_GROUP_N_AMOUNT_OF_TIMES = () => {
+    let repeatNum = "42 42 42";
+    let reRegex = /^(\d+) \1 \1(?!.)/; // Change this line
+    let result = repeatNum.match(reRegex);
+    console.log(result);
+  };
+  CAPTURE_GROUP_N_AMOUNT_OF_TIMES();
+};
+REUSE_PATTERNS_USING_CAPTURE_GROUPS();
+
+const USE_CAPTURES_TO_SEARCH_AND_REPLACE = () => {
+  // can search and replace text in a string using .replace()
+  // eg. wrongText.replace(silverRegex, "blue")
+  // - first param is the regex you want to search
+  // - second param is the string to replace or a func to do something
+  const EXAMPLE_ONE = () => {
+    let wrongText = "The sky is silver.";
+    let silverRegex = /silver/;
+    let answer = wrongText.replace(silverRegex, "orange");
+    console.log(answer);
+  };
+  EXAMPLE_ONE();
+
+  // can also access capture groups with dollar signs. example below
+  const EXAMPLE_TWO = () => {
+    let result = "Code Camp".replace(/(\w+)\s(\w+)/, "$2 $1");
+    console.log(result); // this will flip the two strings
+  };
+  EXAMPLE_TWO();
+
+  let str = "one two three";
+  let fixRegex = /(\w+)\s(\w+)\s(\w+)/; // Change this line
+  let replaceText = "$3 $2 $1"; // Change this line
+  let result = str.replace(fixRegex, replaceText);
+  console.log(result);
+};
+USE_CAPTURES_TO_SEARCH_AND_REPLACE();
